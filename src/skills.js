be698bc8d5913f66a8da273124ed5f2da83b7779
rmf2pt <rmf2pt@gmail.com>
2021-12-10 23:47:29 +0000

    Write code to fetch language data of repos from github api

--- src/skills.js
+++ src/skills.js
@@ -1,9 +1,34 @@
-async function getUserRepos(username) {
-  const repos = await fetch(`https://api.github.com/users/${username}/repos`);
-  return repos;
+async function getUserRepos() {
+  const url = "https://api.github.com/users/rmf2pt/repos"
+  const response = await fetch(url);
+  return result = await response.json();
 }
 
-getUserRepos("rmf2pt")
-     .then(repos => {
-          console.log(repos.name);
-}); 
+async function getRepoLanguages(repo) {
+  const url = `https://api.github.com/repos/rmf2pt/${repo}/languages`;
+  const response = await fetch(url);
+  return result = await response.json();
+}
+
+getUserRepos().then(result => {
+
+  const languages = [];
+  const repos = [];
+  const reposLanguage =[];
+  result.forEach(repo => {
+    repos.push(repo.name);
+    if (!languages.includes(repo.language)) {
+      languages.push(repo.language);
+    }
+  });
+  
+  console.log(repos);
+  
+  repos.forEach(repo => {
+    getRepoLanguages(repo).then(repoLanguage => {
+      reposLanguage.push(repoLanguage); 
+    })
+  })
+
+  console.log(reposLanguage);
+})
